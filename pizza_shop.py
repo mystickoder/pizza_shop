d8022b47f0fe4ae11ed7cb113dba734c1048baf9
mystickoder <89115482+mystickoder@users.noreply.github.com>
2023-01-31 23:00:36 -0500

    pizza shop

---
+++ pizza_shop.py
@@ -0,0 +1,12 @@
+requested_topping = ['mushrooms', 'pepperoni']
+
+if "cheese" in requested_topping:
+    print("Adding cheese")
+    
+if "mushrooms" in requested_topping:
+    print("Adding mushrooms")
+    
+if "pepperoni" in requested_topping:
+    print("Adding pepperoni")
+    
+print(f"Finish making pizza")
